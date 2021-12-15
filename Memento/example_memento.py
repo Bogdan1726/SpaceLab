8954c975027bfc806bbb3ad5a414b6aefd42178a
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2021-12-15 23:34:12 +0200

    edit memento pattern

--- Memento/example_memento.py
+++ Memento/example_memento.py
@@ -1,30 +1,43 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 
 class StateWindows:
     """
     Class state of windows
     """
+    system = 'Windows 10 version 16.07'
+
     def __init__(self):
-        self.state = 'Windows 10'
+        self.state = self.system
 
     def set_state(self, state):
         self.state = state
-
-    def get_state(self):
-        return self.state
+        print(f'Установлена новая версия: {self.state}')
 
     def save_state(self):
         return Memento(self.state)
 
+    def get_state(self):
+        return self.state
+
     def restore_state(self, memento):
         self.state = memento.get_state()
+        print(f'Система восстановила состояния : {self.state}')
 
 
 class IMemento(ABC):
     """
     Abstract class memento
     """
+    @abstractmethod
+    def get_name(self):
+        pass
+
+    @abstractmethod
+    def get_date(self):
+        pass
+
     @abstractmethod
     def get_state(self):
         pass
@@ -36,9 +49,16 @@ class Memento(IMemento):
     """
     def __init__(self, state):
         self.state = state
+        self.date = str(datetime.now())
 
     def get_state(self):
         return self.state
+
+    def get_name(self):
+        return f'{self.date}: {self.state}'
+
+    def get_date(self):
+        return self.date
 
 
 class Caretaker:
@@ -46,21 +66,54 @@ class Caretaker:
     Опекун
     """
     def __init__(self, state_windows: StateWindows):
-        self.memento = None
+        self.mementos = []
         self.state_windows = state_windows
 
     def backup(self):
-        self.memento = self.state_windows.save_state()
+        self.mementos.append(self.state_windows.save_state())
 
-    def get_memento(self):
-        return self.memento
+    def undo(self):
+        if not len(self.mementos):
+            print('Нет больше точек восстановления')
+            return
+        memento = self.mementos.pop()
+        print(f'Восстановление состояния до: {memento.get_name()}')
+        try:
+            self.state_windows.restore_state(memento)
+        except Exception:
+            self.undo()
+
+    # def undo(self, index):
+    #     try:
+    #         memento = self.mementos[index]
+    #         print(f'Восстановление состояния до: {memento.get_name()}')
+    #         self.state_windows.restore_state(memento)
+    #     except IndexError:
+    #         print('Нет точки восстановления с таким номером!')
+
+    def show_history(self):
+        print('Доступные точки восстановления')
+        for memento in self.mementos:
+            print(memento.get_name())
 
 
 windows = StateWindows()
 caretaker = Caretaker(windows)
 caretaker.backup()
-print(windows.get_state())
-windows.set_state('Windows 11')
-print(windows.get_state())
-windows.restore_state(caretaker.get_memento())
+windows.set_state('Windows 10 version 18.10')
+caretaker.backup()
+windows.set_state('Windows 10 version 19.04')
+print()
+caretaker.show_history()
+print()
+
+caretaker.undo()
+caretaker.undo()
+caretaker.undo()
+
+# caretaker.undo(1)
+# caretaker.undo(0)
+# caretaker.undo(1)
+# caretaker.undo(2)
+
 print(windows.get_state())
